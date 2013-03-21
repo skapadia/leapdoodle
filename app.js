672619b0c389a7b7aebed2f2031151e3c091a50b
Sujan Kapadia <skapadia@chariotsolutions.com>
2013-03-21 17:52:24 -0400

    Upload image to Node.js Express App and save PNG to disk

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var engines = require('consolidate');
 var path = require('path');
+var fs = require('fs');
 var app = express();
 
 
@@ -19,6 +20,15 @@ app.configure(function() {
 
 app.get('/eteDoodle', function (req, res) {
    res.render('eteDoodle.html');
+});
+
+app.post('/eteDoodle/saveSnapshot', function (req, res) {
+    var imageDataUrl = req.body.imageData;
+    var indexOfComma = imageDataUrl.indexOf(',');
+    var imageData = imageDataUrl.substring(indexOfComma + 1);
+    console.log("URL Part = " + imageDataUrl.substring(0, indexOfComma));
+    var decodedImage = new Buffer(imageData, 'base64').toString('binary');
+    fs.writeFile('snapshot.png', decodedImage, 'binary', function(err) {});
 });
 
 app.listen(3000);
